869fa9ed3932af547bb93693d768f3021699d6ec
Christyan Duarte <christyanduarte@Christyans-MacBook-Pro-15.local>
2019-03-01 15:33:13 -0600

    CategoriesVC layout completed

---
+++ coder-swag/coder-swag/View/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Christyan Duarte on 3/1/19.
+//  Copyright © 2019 Making your app. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
